1362cf1d120202036b644ce2183d4d68207efaed
Simon Heimberg <simon.heimberg@heimberg-ea.ch>
2023-07-13 17:55:23 +0200

    additional overlay image for valid start positions
    while selecting start position

--- src/wui/interactive_player.cc
+++ src/wui/interactive_player.cc
@@ -623,6 +623,11 @@ void InteractivePlayer::draw_map_view(MapView* given_map_view, RenderTarget* dst
 					blit_field_overlay(
 					   dst, *f, overlay->pic, overlay->hotspot, scale / overlay->scale, opacity);
 				}
+				// addidional mark for start pos
+				if (suited_as_starting_pos) {
+					// overlay is always big, put indicator next to it
+					blit_field_overlay(dst, *f, special_coords_marker_pic_, Vector2i(0, 0), scale, opacity);
+				}
 
 				// Draw port space hint if a port could be built here, but current situation doesn't
 				// allow it.
